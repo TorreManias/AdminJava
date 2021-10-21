dfcf6b08d296ecb4a181308c04558e0eb7e740e2
TorreManias <correosinnombre02@gmail.com>
2021-10-21 16:43:50 -0600

    AdminJAVA

---
+++ src/administracion/Principal.java
@@ -0,0 +1,12 @@
+package administracion;
+
+import GUIs.AdminBoard;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        AdminBoard aBoard = new AdminBoard();
+        aBoard.setVisible(true);
+    }
+    
+}
